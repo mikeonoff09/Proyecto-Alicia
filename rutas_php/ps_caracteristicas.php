7c3622ed42e464c0ed38f676c803b8ff525d9672
Miguel <60907651+Ros4111@users.noreply.github.com>
2021-06-10 00:43:10 +0200

    rutas php

--- rutas_php/ps_caracteristicas.php
+++ rutas_php/ps_caracteristicas.php
@@ -11,12 +11,12 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 function borrar_feature($id_feature,$position,$db){
 	try {
 		//al eliminar un feature hay que corregir la posicion del resto de features
-		$sql = 'update ps_feature set position = position -1 where position > :position';
+		$sql = 'update a_tabla_feature set position = position -1 where position > :position';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
 		$statement->execute();
 
-		$sql = "select id_feature_value,position from ps_feature_value where id_feature =:id_feature";
+		$sql = "select id_feature_value,position from a_tabla_feature_value where id_feature =:id_feature";
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 		$statement->execute();
@@ -25,12 +25,12 @@ function borrar_feature($id_feature,$position,$db){
 		}
 
 		//hay que eliminar cualquier información colocada en un producto, en la tabla feature_value y finalmente en feature
-		$sql = 'delete ps_feature_product.* from ps_feature_product where id_feature = :id_feature';  //borro los valores ya asignados a productos
+		$sql = 'delete a_tabla_feature_product.* from a_tabla_feature_product where id_feature = :id_feature';  //borro los valores ya asignados a productos
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 		$statement->execute();
 
-		$sql = 'delete ps_feature.* from ps_feature where id_feature = :id_feature';
+		$sql = 'delete a_tabla_feature.* from a_tabla_feature where id_feature = :id_feature';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 		$statement->execute();
@@ -44,25 +44,25 @@ function borrar_feature($id_feature,$position,$db){
 function borrar_feature_value($id_feature_value,$position,$db){
 	try {
 		//al eliminar un feature hay que corregir la posicion del resto de features
-		$sql = 'update ps_feature_value set position = position -1 where position > :position';
+		$sql = 'update a_tabla_feature_value set position = position -1 where position > :position';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
 		$statement->execute();
 
 		//marco las fichas de los productos donde se eliminan las feature_value para rehacer el HTML
-		$sql = 'UPDATE a_tabla_product INNER JOIN ps_feature_product ON a_tabla_product.id_product = ps_feature_product.id_product SET a_tabla_product.rehacerHTML = 1
-		WHERE ps_feature_product.id_feature_value=:id_feature_value';
+		$sql = 'UPDATE a_tabla_product INNER JOIN a_tabla_feature_product ON a_tabla_product.id_product = a_tabla_feature_product.id_product SET a_tabla_product.rehacerHTML = 1
+		WHERE a_tabla_feature_product.id_feature_value=:id_feature_value';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_value", $id_feature_value, PDO::PARAM_INT);
 		$statement->execute();
 
 		//hay que eliminar cualquier información colocada en un producto, en la tabla feature_value y finalmente en feature
-		$sql = 'delete ps_feature_product.* from ps_feature_product where id_feature_value = :id_feature_value';  //borro los valores ya asignados a productos
+		$sql = 'delete a_tabla_feature_product.* from a_tabla_feature_product where id_feature_value = :id_feature_value';  //borro los valores ya asignados a productos
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_value", $id_feature_value, PDO::PARAM_INT);
 		$statement->execute();
 
-		$sql = 'delete ps_feature_value.* FROM ps_feature_value WHERE id_feature_value = :id_feature_value';
+		$sql = 'delete a_tabla_feature_value.* FROM a_tabla_feature_value WHERE id_feature_value = :id_feature_value';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_value", $id_feature_value, PDO::PARAM_INT);
 		$statement->execute();
@@ -76,12 +76,12 @@ function borrar_feature_value($id_feature_value,$position,$db){
 
 
 $app->post('/ps_feature_todas/get', function (Request $request, Response $response) {
-	$sql = 'SELECT id_feature,id_feature_super,position,name FROM ps_feature order by position';
+	$sql = 'SELECT id_feature,id_feature_super,position,name FROM a_tabla_feature order by position';
 	try {
 		$dbInstance = new Db();
         $db = $dbInstance->connectDB();
 
-		$sql = 'SELECT id_feature_super,position,name FROM ps_feature_super order by position';
+		$sql = 'SELECT id_feature_super,position,name FROM a_tabla_feature_super order by position';
 		$statement = $db->prepare($sql);
 		$statement->execute();
 		if ($statement->rowCount() == 0) {
@@ -90,7 +90,7 @@ $app->post('/ps_feature_todas/get', function (Request $request, Response $respon
 			$ps_feature_super = $statement->fetchAll(PDO::FETCH_ASSOC);
 		}
 
-		$sql = 'SELECT id_feature,id_feature_super,position,name FROM ps_feature order by position';
+		$sql = 'SELECT id_feature,id_feature_super,position,name FROM a_tabla_feature order by position';
 		$statement = $db->prepare($sql);
 		$statement->execute();
 		if ($statement->rowCount() == 0) {
@@ -99,7 +99,7 @@ $app->post('/ps_feature_todas/get', function (Request $request, Response $respon
 			$ps_feature = $statement->fetchAll(PDO::FETCH_ASSOC);
 		}
 
-		$sql = 'SELECT id_feature_value,id_feature,position,value FROM ps_feature_value';
+		$sql = 'SELECT id_feature_value,id_feature,position,value FROM a_tabla_feature_value';
 		$statement = $db->prepare($sql);
 		$statement->execute();
 		if ($statement->rowCount() == 0) {
@@ -125,25 +125,25 @@ $app->post('/ps_feature/add', function (Request $request, Response $response) {
 	if (!is_numeric($id_feature_super)){
 		return sendResponse(404, null, "id_feature_super no es numero", $response);
 	}
-	$sql = 'SELECT id_feature_super FROM ps_feature_super where id_feature_super = :id_feature_super';
+	$sql = 'SELECT id_feature_super FROM a_tabla_feature_super where id_feature_super = :id_feature_super';
 	$dbInstance = new Db();
 	$db = $dbInstance->connectDB();
 	$statement = $db->prepare($sql);
 	$statement->bindParam(":id_feature_super", $id_feature_super, PDO::PARAM_INT);
 	$statement->execute();
 	if ($statement->rowCount() == 0) {
-		return sendResponse(404, null, "No existe el feature_super en la tabla ps_feature_super", $response);
+		return sendResponse(404, null, "No existe el feature_super en la tabla a_tabla_feature_super", $response);
 	}
 
     try {
-		$sql = 'SELECT COUNT(*) as contador FROM ps_feature where id_feature_super= :id_feature_super'; //al añadir siempre es el último en "position"
+		$sql = 'SELECT COUNT(*) as contador FROM a_tabla_feature where id_feature_super= :id_feature_super'; //al añadir siempre es el último en "position"
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_super", $id_feature_super, PDO::PARAM_INT);
 		$statement->execute();
 		$data = $statement->fetch();
 		$position = $data['contador'];
 
-		$sql = "insert into ps_feature (position,name,id_feature_super) values (:position, :name, :id_feature_super)";
+		$sql = "insert into a_tabla_feature (position,name,id_feature_super) values (:position, :name, :id_feature_super)";
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_super", $id_feature_super, PDO::PARAM_INT);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -177,7 +177,7 @@ $app->post('/ps_feature/update', function (Request $request, Response $response)
 		return sendResponse(404, null, "No has enviado el nombre", $response);
 	}
 
-	$sql = 'SELECT position,id_feature_super FROM ps_feature where id_feature = :id_feature';
+	$sql = 'SELECT position,id_feature_super FROM a_tabla_feature where id_feature = :id_feature';
 	$dbInstance = new Db();
     $db = $dbInstance->connectDB();
 	$statement = $db->prepare($sql);
@@ -194,9 +194,9 @@ $app->post('/ps_feature/update', function (Request $request, Response $response)
 	// 0,1,2,3,4,5, etc
 	if ($positionantigua != $position){
 		if ($positionantigua > $position){
-			$sql = "update ps_feature set position=position +1 where id_feature_super = :id_feature_super and position >= :position and position < :positionantigua";
+			$sql = "update a_tabla_feature set position=position +1 where id_feature_super = :id_feature_super and position >= :position and position < :positionantigua";
 		}else{
-			$sql = "update ps_feature set position=position -1 where id_feature_super = :id_feature_super and position >= :positionantigua and position <= :position";
+			$sql = "update a_tabla_feature set position=position -1 where id_feature_super = :id_feature_super and position >= :positionantigua and position <= :position";
 		}
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -205,7 +205,7 @@ $app->post('/ps_feature/update', function (Request $request, Response $response)
 	}
 
     try {
-		$sql = "UPDATE ps_feature SET position=:position, name=:name WHERE id_feature = :id_feature";
+		$sql = "UPDATE a_tabla_feature SET position=:position, name=:name WHERE id_feature = :id_feature";
         $statement = $db->prepare($sql);
         $statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
         $statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -231,7 +231,7 @@ $app->post('/ps_feature/delete', function (Request $request, Response $response)
 	}
 	$dbInstance = new Db();
     $db = $dbInstance->connectDB();
-	$sql = 'SELECT position FROM ps_feature where id_feature = :id_feature';
+	$sql = 'SELECT position FROM a_tabla_feature where id_feature = :id_feature';
 	$statement = $db->prepare($sql);
 	$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 	$statement->execute();
@@ -270,23 +270,23 @@ $app->post('/ps_feature_value/add', function (Request $request, Response $respon
 	$dbInstance = new Db();
 	$db = $dbInstance->connectDB();
 
-	$sql = 'SELECT id_feature FROM ps_feature where id_feature = :id_feature';
+	$sql = 'SELECT id_feature FROM a_tabla_feature where id_feature = :id_feature';
 	$statement = $db->prepare($sql);
 	$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 	$statement->execute();
 	if ($statement->rowCount() == 0) {
-		return sendResponse(404, null, "No existe el id_feature en la tabla ps_feature", $response);
+		return sendResponse(404, null, "No existe el id_feature en la tabla a_tabla_feature", $response);
 	}
 
     try {
 
-		$sql = 'SELECT COUNT(*) as contador FROM ps_feature_value'; //al añadir siempre es el último en "position"
+		$sql = 'SELECT COUNT(*) as contador FROM a_tabla_feature_value'; //al añadir siempre es el último en "position"
 		$statement = $db->prepare($sql);
 		$statement->execute();
 		$data = $statement->fetch();
 		$position = $data['contador'];
 
-		$sql = "insert into ps_feature_value (position,value,id_feature) values (:position, :value, :id_feature)";
+		$sql = "insert into a_tabla_feature_value (position,value,id_feature) values (:position, :value, :id_feature)";
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -320,7 +320,7 @@ $app->post('/ps_feature_value/update', function (Request $request, Response $res
 		return sendResponse(404, null, "No has enviado el value", $response);
 	}
 
-	$sql = 'SELECT position,id_feature FROM ps_feature_value where id_feature_value = :id_feature_value';
+	$sql = 'SELECT position,id_feature FROM a_tabla_feature_value where id_feature_value = :id_feature_value';
 	$dbInstance = new Db();
     $db = $dbInstance->connectDB();
 	$statement = $db->prepare($sql);
@@ -337,9 +337,9 @@ $app->post('/ps_feature_value/update', function (Request $request, Response $res
 	// 0,1,2,3,4,5, etc
 	if ($positionantigua != $position){
 		if ($positionantigua > $position){
-			$sql = "update ps_feature_value set position=position +1 where id_feature = :id_feature and position >= :position and position < :positionantigua";
+			$sql = "update a_tabla_feature_value set position=position +1 where id_feature = :id_feature and position >= :position and position < :positionantigua";
 		}else{
-			$sql = "update ps_feature_value set position=position -1 where id_feature = :id_feature and position >= :positionantigua and position <= :position";
+			$sql = "update a_tabla_feature_value set position=position -1 where id_feature = :id_feature and position >= :positionantigua and position <= :position";
 		}
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
@@ -349,7 +349,7 @@ $app->post('/ps_feature_value/update', function (Request $request, Response $res
 	}
 
     try {
-		$sql = "UPDATE ps_feature_value SET position=:position, value=:value WHERE id_feature_value = :id_feature_value";
+		$sql = "UPDATE a_tabla_feature_value SET position=:position, value=:value WHERE id_feature_value = :id_feature_value";
         $statement = $db->prepare($sql);
         $statement->bindParam(":id_feature_value", $id_feature, PDO::PARAM_INT);
         $statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -375,7 +375,7 @@ $app->post('/ps_feature_value/delete', function (Request $request, Response $res
 	}
 	$dbInstance = new Db();
     $db = $dbInstance->connectDB();
-	$sql = 'SELECT position FROM ps_feature_value where id_feature_value = :id_feature_value';
+	$sql = 'SELECT position FROM a_tabla_feature_value where id_feature_value = :id_feature_value';
 	$statement = $db->prepare($sql);
 	$statement->bindParam(":id_feature_value", $id_feature_value, PDO::PARAM_INT);
 	$statement->execute();
@@ -412,13 +412,13 @@ $app->post('/ps_feature_super/add', function (Request $request, Response $respon
         $dbInstance = new Db();
         $db = $dbInstance->connectDB();
 
-		$sql = 'SELECT COUNT(*) as contador FROM ps_feature_super'; //al añadir siempre es el último en "position"
+		$sql = 'SELECT COUNT(*) as contador FROM a_tabla_feature_super'; //al añadir siempre es el último en "position"
 		$statement = $db->prepare($sql);
 		$statement->execute();	   
 		$data = $statement->fetch();
 		$position = $data['contador'];
 
-		$sql = "insert into ps_feature_super (position,name) values (:position, :name)";
+		$sql = "insert into a_tabla_feature_super (position,name) values (:position, :name)";
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
 		$statement->bindParam(":name", $name,PDO::PARAM_STR);
@@ -451,7 +451,7 @@ $app->post('/ps_feature_super/update', function (Request $request, Response $res
 		return sendResponse(404, null, "No has enviado el nombre", $response);
 	}
 
-	$sql = 'SELECT position FROM ps_feature_super where id_feature_super = :id_feature_super';
+	$sql = 'SELECT position FROM a_tabla_feature_super where id_feature_super = :id_feature_super';
 	$dbInstance = new Db();
     $db = $dbInstance->connectDB();
 	$statement = $db->prepare($sql);
@@ -467,9 +467,9 @@ $app->post('/ps_feature_super/update', function (Request $request, Response $res
 	// 0,1,2,3,4,5, etc
 	if ($positionantigua != $position){
 		if ($positionantigua > $position){
-			$sql = "update ps_feature_super set position=position +1 where position >= :position and position < :positionantigua";
+			$sql = "update a_tabla_feature_super set position=position +1 where position >= :position and position < :positionantigua";
 		}else{
-			$sql = "update ps_feature_super set position=position -1 where position >= :positionantigua and position <= :position";
+			$sql = "update a_tabla_feature_super set position=position -1 where position >= :positionantigua and position <= :position";
 		}
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -478,7 +478,7 @@ $app->post('/ps_feature_super/update', function (Request $request, Response $res
 	}
 
     try {
-		$sql = "UPDATE ps_feature_super SET position=:position, name=:name WHERE id_feature_super = :id_feature_super";
+		$sql = "UPDATE a_tabla_feature_super SET position=:position, name=:name WHERE id_feature_super = :id_feature_super";
         $statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_super", $id_feature_super, PDO::PARAM_INT);
         $statement->bindParam(":position", $position, PDO::PARAM_INT);
@@ -504,7 +504,7 @@ $app->post('/ps_feature_super/delete', function (Request $request, Response $res
 	}
 	$dbInstance = new Db();
     $db = $dbInstance->connectDB();
-	$sql = 'SELECT position FROM ps_feature_super where id_feature_super = :id_feature_super';
+	$sql = 'SELECT position FROM a_tabla_feature_super where id_feature_super = :id_feature_super';
 	$statement = $db->prepare($sql);
 	$statement->bindParam(":id_feature_super", $id_feature_super, PDO::PARAM_INT);
 	$statement->execute();
@@ -516,12 +516,12 @@ $app->post('/ps_feature_super/delete', function (Request $request, Response $res
 
 	try {
 		//al eliminar un feature hay que corregir la posicion del resto de features
-		$sql = 'update ps_feature_super set position = position -1 where position > :position';
+		$sql = 'update a_tabla_feature_super set position = position -1 where position > :position';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":position", $position, PDO::PARAM_INT);
 		$statement->execute();
 
-		$sql = "select id_feature,position from ps_feature where id_feature_super =:id_feature_super";
+		$sql = "select id_feature,position from a_tabla_feature where id_feature_super =:id_feature_super";
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature", $id_feature, PDO::PARAM_INT);
 		$statement->execute();
@@ -529,7 +529,7 @@ $app->post('/ps_feature_super/delete', function (Request $request, Response $res
 			borrar_feature($fila['id_feature'],$fila['position'],$db);
 		}
 		
-		$sql = 'DELETE ps_feature_super.* FROM ps_feature_super WHERE id_feature_super=:id_feature_super';
+		$sql = 'DELETE a_tabla_feature_super.* FROM a_tabla_feature_super WHERE id_feature_super=:id_feature_super';
 		$statement = $db->prepare($sql);
 		$statement->bindParam(":id_feature_super", $id_feature_super, PDO::PARAM_INT);
 		$statement->execute();
@@ -546,17 +546,17 @@ $app->post('/ps_feature/get_todos', function (Request $request, Response $respon
 		$dbInstance = new Db();
         $db = $dbInstance->connectDB();
 
-		$sql = 'SELECT id_feature_super,position,name FROM ps_feature_super order by position';
+		$sql = 'SELECT id_feature_super,position,name FROM a_tabla_feature_super order by position';
 		$statement = $db->prepare($sql);
 		$statement->execute();
 		$ps_feature_super = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-		$sql = 'SELECT id_feature,id_feature_super,position,name FROM ps_feature order by position';
+		$sql = 'SELECT id_feature,id_feature_super,position,name FROM a_tabla_feature order by position';
 		$statement = $db->prepare($sql);
 		$statement->execute();
 		$ps_feature = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-		$sql = 'SELECT id_feature_value,id_feature,position,value FROM ps_feature_value';
+		$sql = 'SELECT id_feature_value,id_feature,position,value FROM a_tabla_feature_value';
 		$dbInstance = new Db();
         $db = $dbInstance->connectDB();
 		$statement = $db->prepare($sql);
