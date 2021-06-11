02b6cd23e41dfa699c1d0b01b940b45208daf494
Miguel <60907651+Ros4111@users.noreply.github.com>
2021-06-11 23:06:35 +0200

    creadas clases para features descargadas junto al producto

--- lib/src/models/product_details_model.dart
+++ lib/src/models/product_details_model.dart
@@ -8,12 +8,18 @@ class ProductDetailsMode extends Equatable {
     this.psImage,
     this.psCategoryProduct,
     this.psFeatureProduct,
+    this.psFeatureSuper,
+    this.psFeature,
+    this.psFeatureValue,
   });
 
   List<PsProduct> psProduct;
   List<PsImage> psImage;
   List<PsCategoryProduct> psCategoryProduct;
   List<PsFeatureProduct> psFeatureProduct;
+  List<PsFeatureSuper> psFeatureSuper;
+  List<PsFeature> psFeature;
+  List<PsFeatureValue> psFeatureValue;
 
   factory ProductDetailsMode.fromMap(Map<String, dynamic> json) =>
       ProductDetailsMode(
@@ -26,6 +32,12 @@ class ProductDetailsMode extends Equatable {
             List<PsImage>.from(json["ps_image"].map((x) => PsImage.fromMap(x))),
         psFeatureProduct: List<PsFeatureProduct>.from(
             json["ps_feature_product"].map((x) => PsFeatureProduct.fromMap(x))),
+        psFeatureSuper: List<PsFeatureSuper>.from(
+            json["ps_feature_super"].map((x) => PsFeatureSuper.fromMap(x))),
+        psFeature: List<PsFeature>.from(
+            json["ps_feature"].map((x) => PsFeature.fromMap(x))),
+        psFeatureValue: List<PsFeatureValue>.from(
+            json["ps_feature_value"].map((x) => PsFeatureValue.fromMap(x))),
       );
 
   Map<String, dynamic> toMap() => {
@@ -35,6 +47,11 @@ class ProductDetailsMode extends Equatable {
             List<dynamic>.from(psCategoryProduct.map((x) => x.toMap())),
         "ps_feature_product":
             List<dynamic>.from(psFeatureProduct.map((x) => x.toMap())),
+        "ps_feature_super":
+            List<dynamic>.from(psFeatureSuper.map((x) => x.toMap())),
+        "ps_feature": List<dynamic>.from(psFeature.map((x) => x.toMap())),
+        "ps_feature_value":
+            List<dynamic>.from(psFeatureValue.map((x) => x.toMap())),
       };
 
   @override
@@ -43,6 +60,9 @@ class ProductDetailsMode extends Equatable {
         psImage,
         psCategoryProduct,
         psFeatureProduct,
+        psFeatureSuper,
+        psFeature,
+        psFeatureValue,
       ];
 }
 
@@ -65,6 +85,109 @@ class PsCategoryProduct extends Equatable {
 
   @override
   List<Object> get props => [idCategory];
+}
+
+class PsFeatureSuper extends Equatable {
+  PsFeatureSuper({
+    this.idFeatureSuper,
+    this.position,
+    this.name,
+  });
+
+  int idFeatureSuper;
+  int position;
+  String name;
+
+  factory PsFeatureSuper.fromMap(Map<String, dynamic> json) => PsFeatureSuper(
+        idFeatureSuper: json["id_feature_super"],
+        position: json["position"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id_feature_super": idFeatureSuper,
+        "position": position,
+        "name": name,
+      };
+
+  @override
+  List<Object> get props => [
+        idFeatureSuper,
+        position,
+        name,
+      ];
+}
+
+class PsFeature extends Equatable {
+  PsFeature({
+    this.idFeature,
+    this.idFeatureSuper,
+    this.position,
+    this.name,
+  });
+
+  int idFeature;
+  int idFeatureSuper;
+  int position;
+  String name;
+
+  factory PsFeature.fromMap(Map<String, dynamic> json) => PsFeature(
+        idFeature: json["id_feature"],
+        idFeatureSuper: json["id_feature_super"],
+        position: json["position"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id_feature": idFeature,
+        "id_feature_super": idFeatureSuper,
+        "position": position,
+        "name": name,
+      };
+
+  @override
+  List<Object> get props => [
+        idFeature,
+        idFeatureSuper,
+        position,
+        name,
+      ];
+}
+
+class PsFeatureValue extends Equatable {
+  PsFeatureValue({
+    this.idFeatureValue,
+    this.idFeature,
+    this.position,
+    this.name,
+  });
+
+  int idFeatureValue;
+  int idFeature;
+  int position;
+  String name;
+
+  factory PsFeatureValue.fromMap(Map<String, dynamic> json) => PsFeatureValue(
+        idFeatureValue: json["id_feature_value"],
+        idFeature: json["id_feature"],
+        position: json["position"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id_feature_value": idFeatureValue,
+        "id_feature": idFeature,
+        "position": position,
+        "name": name,
+      };
+
+  @override
+  List<Object> get props => [
+        idFeatureValue,
+        idFeature,
+        position,
+        name,
+      ];
 }
 
 // ignore: must_be_immutable
