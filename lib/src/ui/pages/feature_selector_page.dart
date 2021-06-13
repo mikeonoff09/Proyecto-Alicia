afa5e0fbd862006d554eb2828d087a92da7bcf79
mikeonoff09 <48326837+mikeonoff09@users.noreply.github.com>
2021-06-13 00:25:45 -0500

    Search and Selection done

--- lib/src/ui/pages/feature_selector_page.dart
+++ lib/src/ui/pages/feature_selector_page.dart
@@ -20,6 +20,10 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
   final TextEditingController featureController = TextEditingController();
   final TextEditingController featureValueController = TextEditingController();
 
+  List<PsFeatureValue> psFeatureValueList;
+  List<PsFeatureSuper> psFeatureSuperList;
+  List<PsFeature> psFeatureList;
+
   @override
   Widget build(BuildContext context) {
     final featureProductBloc = BlocProvider.of<ProductBloc>(context);
@@ -41,9 +45,11 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
   }
 
   Container featuresLists() {
-    List<PsFeatureValue> psFeatureValueList = widget.productData.psFeatureValue;
-    List<PsFeatureSuper> psFeatureSuperlist = widget.productData.psFeatureSuper;
-    List<PsFeature> psFeatureList = widget.productData.psFeature;
+    psFeatureValueList =
+        psFeatureValueList ?? widget.productData.psFeatureValue;
+    psFeatureSuperList =
+        psFeatureSuperList ?? widget.productData.psFeatureSuper;
+    psFeatureList = psFeatureList ?? widget.productData.psFeature;
     final featureProductBloc = BlocProvider.of<ProductBloc>(context);
     return Container(
       height: MediaQuery.of(context).size.height * 0.5,
@@ -54,14 +60,12 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
           Expanded(
             flex: 1,
             child: ListView.builder(
-              itemCount: psFeatureSuperlist.length,
+              itemCount: psFeatureSuperList.length,
               itemBuilder: (BuildContext context, int index) {
                 return ListTile(
-                  title: Text(psFeatureSuperlist[index].name),
-                  onTap: () {
-                    // TODO: cambiar los datos de las listas
-                  },
-                );
+                    title: Text(psFeatureSuperList[index].name),
+                    onTap: () => _newFeatureSuperSelected(
+                        psFeatureSuperList[index].idFeatureSuper));
               },
             ),
           ),
@@ -71,11 +75,9 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
               itemCount: psFeatureList.length,
               itemBuilder: (BuildContext context, int index) {
                 return ListTile(
-                  title: Text(psFeatureList[index].name),
-                  onTap: (){
-                    // TODO: Cambiar los datos de las listas
-                  },
-                );
+                    title: Text(psFeatureList[index].name),
+                    onTap: () =>
+                        _newFeatureSelected(psFeatureList[index].idFeature));
               },
             ),
           ),
@@ -86,9 +88,11 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
               itemBuilder: (BuildContext context, int index) {
                 return ListTile(
                   title: Text(psFeatureValueList[index].name),
-                  onTap: (){
+                  onTap: () {
                     // TODO: Marcar la propiedad como seleccionada
                   },
+                  trailing:
+                      IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
                 );
               },
             ),
@@ -98,12 +102,49 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
     );
   }
 
+  _newFeatureSuperSelected(int idFeatureSuper) {
+    print("New Feature Super Clicked ");
+    psFeatureValueList = [];
+    psFeatureList = [];
+
+    for (var feature in widget.productData.psFeature) {
+      if (feature.idFeatureSuper == idFeatureSuper) {
+        psFeatureList.add(feature);
+        for (var featureValue in widget.productData.psFeatureValue) {
+          if (featureValue.idFeature == feature.idFeature) {
+            print("adding");
+            psFeatureValueList.add(featureValue);
+          }
+        }
+      }
+    }
+    setState(() {});
+  }
+
+  _newFeatureSelected(int idFeature) {
+    print("New Feature Clicked ");
+    psFeatureValueList = [];
+
+    for (var featureValue in widget.productData.psFeatureValue) {
+      if (featureValue.idFeature == idFeature) {
+        print("adding");
+        psFeatureValueList.add(featureValue);
+      }
+    }
+    setState(() {});
+  }
+
   Container mainSearch() {
     return Container(
       // height: 120,
       child: MyTextField(
         controller: searchController,
-        labelText: "Busqueda",
+        labelText: "Busqueda General",
+        onChanged: (value) {
+          _searchFeature(value);
+          _searchFeatureSuper(value);
+          _searchFeatureValue(value);
+        },
       ),
     );
   }
@@ -116,27 +157,81 @@ class _FeatureSelectorPageState extends State<FeatureSelectorPage> {
         children: [
           Expanded(
             flex: 1,
-            child: MyTextField(
-              controller: featureSuperController,
-              labelText: "Busqueda en Feature Super",
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyTextField(
+                    controller: featureSuperController,
+                    labelText: "Busqueda en Feature Super",
+                    onChanged: (value) => _searchFeatureSuper(value),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              ],
             ),
           ),
           Expanded(
             flex: 1,
-            child: MyTextField(
-              controller: featureController,
-              labelText: "Busqueda en Feature",
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyTextField(
+                    controller: featureController,
+                    labelText: "Busqueda en Feature",
+                    onChanged: (value) => _searchFeature(value),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              ],
             ),
           ),
           Expanded(
             flex: 1,
-            child: MyTextField(
-              controller: featureValueController,
-              labelText: "Busqueda en Feature Value",
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyTextField(
+                    controller: featureValueController,
+                    labelText: "Busqueda en Feature Value",
+                    onChanged: (value) => _searchFeatureValue(value),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              ],
             ),
           ),
         ],
       ),
     );
+  }
+
+  _searchFeatureSuper(String value) {
+    psFeatureSuperList = [];
+    for (var featureSuper in widget.productData.psFeatureSuper) {
+      if (featureSuper.name.toLowerCase().contains(value.toLowerCase())) {
+        psFeatureSuperList.add(featureSuper);
+      }
+    }
+    setState(() {});
+  }
+
+  _searchFeature(String value) {
+    psFeatureList = [];
+    for (var feature in widget.productData.psFeature) {
+      if (feature.name.toLowerCase().contains(value.toLowerCase())) {
+        psFeatureList.add(feature);
+      }
+    }
+    setState(() {});
+  }
+
+  _searchFeatureValue(String value) {
+    psFeatureValueList = [];
+    for (var featureValue in widget.productData.psFeatureValue) {
+      if (featureValue.name.toLowerCase().contains(value.toLowerCase())) {
+        psFeatureValueList.add(featureValue);
+      }
+    }
+    setState(() {});
   }
 }
